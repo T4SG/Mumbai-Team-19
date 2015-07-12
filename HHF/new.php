048bed8e8cbc4fe02d843158a173555500610058
anne06 <anooshasagar@gmail.com>
2015-07-12 06:20:14 +0530

    new data

---
+++ HHF/new.php
@@ -0,0 +1,336 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta name="description" content="Meghna One page parallax responsive HTML Template ">
+        
+        <meta name="author" content="Muhammad Morshed">
+		
+        <title>Meghna | Responsive Multipurpose Parallax HTML5 Template</title>
+		
+		<!-- Mobile Specific Meta
+		================================================== -->
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+		
+		<!-- Favicon -->
+		<link rel="shortcut icon" type="image/x-icon" href="img/favicon.png" />
+		
+		<!-- CSS
+		================================================== -->
+		<!-- Fontawesome Icon font -->
+        <link rel="stylesheet" href="css/font-awesome.min.css">
+		<!-- bootstrap.min css -->
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+		<!-- Animate.css -->
+        <link rel="stylesheet" href="css/animate.css">
+		<!-- Owl Carousel -->
+        <link rel="stylesheet" href="css/owl.carousel.css">
+		<!-- Grid Component css -->
+        <link rel="stylesheet" href="css/component.css">
+		<!-- Slit Slider css -->
+        <link rel="stylesheet" href="css/slit-slider.css">
+		<!-- Main Stylesheet -->
+        <link rel="stylesheet" href="css/main.css">
+		<!-- Media Queries -->
+        <link rel="stylesheet" href="css/media-queries.css">
+
+		<!--
+		Google Font
+		=========================== -->                    
+		
+		<!-- Oswald / Title Font -->
+		<link href='http://fonts.googleapis.com/css?family=Oswald:400,300' rel='stylesheet' type='text/css'>
+		<!-- Ubuntu / Body Font -->
+		<link href='http://fonts.googleapis.com/css?family=Ubuntu:400,300' rel='stylesheet' type='text/css'>
+		
+		<!-- Modernizer Script for old Browsers -->		
+        <script src="js/modernizr-2.6.2.min.js"></script>
+
+
+		<script>
+		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+		
+		  ga('create', 'UA-54152927-1', 'auto');
+		  ga('send', 'pageview');
+		
+		</script>
+	
+    </head>
+	
+    <body id="body">
+	    <!--
+	    Start Preloader
+	    ==================================== -->
+	<!--	<div id="loading-mask">
+			<div class="loading-img">
+				<img alt="Meghna Preloader" src="img/preloader.gif"  />
+			</div>
+		</div>-->
+        <!--
+        End Preloader
+        ==================================== -->
+		
+        <!--
+        Welcome Slider
+        ==================================== -->
+		<section id="home">	    
+		
+            <div id="slitSlider" class="sl-slider-wrapper">
+				<div class="sl-slider">
+					
+					<!-- single slide item -->
+					<div class="sl-slide" data-orientation="horizontal" data-slice1-rotation="-25" data-slice2-rotation="-25" data-slice1-scale="2" data-slice2-scale="2">
+						<div class="sl-slide-inner">
+							<div class="bg-img bg-img-1"></div>
+						<div class="carousel-caption">
+							<div>
+								<img class="wow fadeInUp" src="img/meghna.png" alt="Meghna">
+								<h2 data-wow-duration="500ms"  data-wow-delay="500ms" class="heading animated fadeInRight">Welcome To Meghna</h2>
+							</div>
+						</div>
+						</div>
+					</div>
+					<!-- /single slide item -->
+					
+					<!-- single slide item -->
+					<div class="sl-slide" data-orientation="vertical" data-slice1-rotation="10" data-slice2-rotation="-15" data-slice1-scale="1.5" data-slice2-scale="1.5">
+						<div class="sl-slide-inner">
+							<div class="bg-img bg-img-2"></div>
+						<div class="carousel-caption">
+							<div>
+								<h2 class="heading animated fadeInDown">Highly Responsive</h2>
+								<h3 class="animated fadeInUp">With all Mejor Browser support including IE 9</h3>
+								<a class="btn btn-green animated fadeInUp" href="#">Get Started</a>
+							</div>
+						</div>
+						</div>
+					</div>
+					<!-- /single slide item -->
+					
+					<!-- single slide item -->
+					<div class="sl-slide" data-orientation="horizontal" data-slice1-rotation="3" data-slice2-rotation="3" data-slice1-scale="2" data-slice2-scale="1">
+						<div class="sl-slide-inner">
+							<div class="bg-img bg-img-3"></div>
+						<div class="carousel-caption">
+							<div>
+								<h2 class="heading animated fadeInRight">One Page Parallax HTML Template</h2>
+								<h3 class="animated fadeInLeft">Clean and Professional Design</h3>
+								<a class="btn btn-green animated fadeInUp" href="#">Learn More</a>
+							</div>
+						</div>
+						</div>
+					</div>
+					<!-- /single slide item -->
+
+				</div><!-- /sl-slider -->
+				
+				<nav id="nav-arrows" class="nav-arrows">
+					<span class="nav-arrow-prev">Previous</span>
+					<span class="nav-arrow-next">Next</span>
+				</nav>
+
+				<nav id="nav-dots" class="nav-dots">
+					<span class="nav-dot-current"></span>
+					<span></span>
+					<span></span>
+				</nav>
+
+			</div><!-- /slider-wrapper -->
+		</section>
+		<!--/#home section-->
+		
+        <!-- 
+        Fixed Navigation
+        ==================================== -->
+        <header id="navigation" class="navbar navbar-inverse">
+            <div class="container">
+                <div class="navbar-header">
+                    <!-- responsive nav button -->
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+					<!-- /responsive nav button -->
+					
+					<!-- logo -->
+                    <a class="navbar-brand" href="#body">
+						<h1 id="logo">
+							<img src="img/logo-meghna.png" alt="Meghna" />
+						</h1>
+					</a>
+					<!-- /logo -->
+                </div>
+
+				<!-- main nav -->
+                <nav class="collapse navbar-collapse navbar-right" role="Navigation">
+                    <ul id="nav" class="nav navbar-nav">
+                        <li class="current"><a href="#body">Home</a></li>
+                        <li><a href="#main-features">Cardio Rush Challenge</a></li>
+                        <li><a href="#services">Schools</a></li>
+                        <li><a href="#showcase">Portfolio</a></li>
+                        <li><a href="#our-team">Team</a></li>
+                        <li><a href="#pricing">Pricing</a></li>
+                        <li><a href="#blog">Blog</a></li>
+                        <li><a href="#contact-us">Contact</a></li>
+                    </ul>
+                </nav>
+				<!-- /main nav -->
+				
+            </div>
+        </header>
+        
+		
+		<!--
+		Start Main Features
+		==================================== -->
+		<section id="main-features">
+			<div class="container">
+				<div class="row">
+					
+					<!-- features item -->
+					<div id="features">
+						<div class="item">
+							
+							<div class="features-item">
+								
+								<!-- features media -->
+								<div class="col-md-6 feature-media media-wrapper wow fadeInUp" data-wow-duration="500ms">
+									<img src="img/2.jpg" alt="band" >
+								</div>
+								<!-- /features media -->
+								
+								<!-- features content -->
+								<div class="col-md-6 feature-desc wow fadeInUp" data-wow-duration="500ms" data-wow-delay="300ms">
+									<h3>Our Innovative Venture by JP Morgan Chase</h3>
+						
+								</div>
+								<!-- /features content -->
+								
+							</div>
+						</div>
+						
+						
+					
+				</div> 		<!-- End row -->
+			</div>   	<!-- End container -->
+		</section>   <!-- End section -->
+		
+		
+		
+		
+		<!-- Start Services Section
+		==================================== -->
+		<?php $connect=mysqli_connect('localhost', 'root', '','hhf') or trigger_error("Unable to connect to the database: " . mysqli_error()); ?>
+		<section id="services" class="bg-one">
+			<div class="container">
+					
+					<!-- section title -->
+					<div class="title text-center wow fadeIn" data-wow-duration="500ms">
+						<h2>Our <span class="color">Schools</span></h2>
+						<div class="border"></div>
+					</div>
+					<!-- /section title -->
+					
+                    <center><table style= "border-radius: 25px; 
+    border: 1px solid #8AC007; 
+    padding: 20px; 
+    width: 900px;
+    height: 150px; "> 
+
+
+
+    
+                    <tr align="center" style="border: 1px solid; color: #8AC007;" border: 1px solid #8AC007;><td>School Name</td>
+                    	<td>Location</td>
+                    	<td>Calamity</td>
+                    	<td>Year</td>
+                    	<td>Total Cost for Rebuild</td>
+                    	<td>Money Needed</td>
+                    	<td>Progress</td>
+                    	<td></td>
+                    </tr>
+                    <?php
+                    $result=mysqli_query($connect,"SELECT * FROM school s,school_stage ss where s.ss_id=ss.ss_id");
+                    $i=1;
+                    while($row=mysqli_fetch_array($result))
+                    {
+                    	?>
+                    <tr align="center" >
+                    <td><?php echo $row['s_name']; ?></td>
+                    <td><?php echo $row['location']; ?></td>
+                    <td><?php echo $row['calamity']; ?></td>
+                    <td><?php echo $row['c_year']; ?></td>
+                    <td><?php echo $row['d_amount']; ?></td>
+                    <td><?php echo $row['m_required']; ?></td>
+                  	<td><?php echo $row['ss_perc']; ?></td>
+                  	<td><form action="donatetohhf.php" method="post"><input type="submit" value="Donate" style="color:black"></form>  </td>
+                    </tr>
+                    <?php 
+                    $i++;}
+                    ?></table>
+                    </center>
+						
+				
+			</div>   	<!-- End container -->
+		</section>   <!-- End section -->
+		
+		
+		
+		
+		<!-- end Footer Area
+		========================================== -->
+		
+		<!-- 
+		Essential Scripts
+		=====================================-->
+		
+		<!-- Main jQuery -->
+		<script src="js/jquery-1.11.0.min.js"></script>
+		<!-- Bootstrap 3.1 -->
+		<script src="js/bootstrap.min.js"></script>
+		<!-- Slitslider -->
+		<script src="js/jquery.slitslider.js"></script>
+		<script src="js/jquery.ba-cond.min.js"></script>
+		<!-- Parallax -->
+		<script src="js/jquery.parallax-1.1.3.js"></script>
+		<!-- Owl Carousel -->
+		<script src="js/owl.carousel.min.js"></script>
+		<!-- Portfolio Filtering -->
+		<script src="js/jquery.mixitup.min.js"></script>
+		<!-- Custom Scrollbar -->
+		<script src="js/jquery.nicescroll.min.js"></script>
+		<!-- Jappear js -->
+		<script src="js/jquery.appear.js"></script>
+		<!-- Pie Chart -->
+		<script src="js/easyPieChart.js"></script>
+		<!-- jQuery Easing -->
+		<script src="js/jquery.easing-1.3.pack.js"></script>
+		<!-- tweetie.min -->
+		<script src="js/tweetie.min.js"></script>
+		<!-- Google Map API -->
+		<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
+		<!-- Highlight menu item -->
+		<script src="js/jquery.nav.js"></script>
+		<!-- Sticky Nav -->
+		<script src="js/jquery.sticky.js"></script>
+		<!-- Number Counter Script -->
+		<script src="js/jquery.countTo.js"></script>
+		<!-- wow.min Script -->
+		<script src="js/wow.min.js"></script>
+		<!-- For video responsive -->
+		<script src="js/jquery.fitvids.js"></script>
+		<!-- Grid js -->
+		<script src="js/grid.js"></script>
+		<!-- Custom js -->
+		<script src="js/custom.js"></script>
+
+    </body>
+</html>
